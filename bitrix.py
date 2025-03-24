797c7bbfa0b70bbf13191f6ef846f86c53b163e9
mixma5ter <deejay999@yandex.ru>
2025-03-24 18:48:32 +0300

    bug fixes

--- bitrix.py
+++ bitrix.py
@@ -4,6 +4,7 @@ import uuid
 import requests
 
 from dotenv import load_dotenv
+from requests import Timeout
 
 load_dotenv()
 WEB_HOOK = os.getenv('WEB_HOOK')
@@ -15,16 +16,24 @@ SOCNET_GROUP_ID = 22
 LIST_TEMPLATE = 'mcko.bitrix24.ru/workgroups/group/{}/lists/{}/view/0/'.format(SOCNET_GROUP_ID, '{}')
 
 
-def create_element(event, fields):
+def create_element(event, fields, timeout=10):
     """Создает элемент в списке event в Битрикс."""
 
-    command = 'lists.element.add'
-    url = WEB_HOOK + command
-    params = {
-        'IBLOCK_TYPE_ID': IBLOCK_TYPE_ID,
-        'SOCNET_GROUP_ID': SOCNET_GROUP_ID,
-        'IBLOCK_CODE': IBLOCK_CODE.format(event),
-        'ELEMENT_CODE': uuid.uuid1().int,
-        'FIELDS': fields
-    }
-    return requests.post(url, json=params)
+    try:
+        command = 'lists.element.add'
+        url = WEB_HOOK + command
+        params = {
+            'IBLOCK_TYPE_ID': IBLOCK_TYPE_ID,
+            'SOCNET_GROUP_ID': SOCNET_GROUP_ID,
+            'IBLOCK_CODE': IBLOCK_CODE.format(event),
+            'ELEMENT_CODE': uuid.uuid1().int,
+            'FIELDS': fields
+        }
+        response = requests.post(url, json=params, timeout=timeout)
+        response.raise_for_status()  # Поднимает исключение для неудачных кодов состояния (4xx или 5xx)
+        return response
+
+    except Timeout:
+        return f"Error timeout creating element in Bitrix24 (event {event}), Fields: {fields}"
+    except Exception as e:
+        return f"Unexpected error creating element in Bitrix24 (event {event}): {e}, Fields: {fields}"
--- card_reader.py
+++ card_reader.py
@@ -28,6 +28,15 @@ parity = serial.PARITY_NONE
 stopbits = serial.STOPBITS_ONE
 
 
+def read_card_data_non_blocking(ser):
+    """Читает данные с последовательного порта неблокирующим способом."""
+    data = ser.in_waiting
+    if data > 0:
+      line = ser.read(data).decode('ascii', errors='ignore').strip()
+      return line
+    return None
+
+
 def get_and_patch_user_data(card_number):
     logging.debug(f"Processing card number: {card_number}")
 
@@ -66,6 +75,7 @@ def get_and_patch_user_data(card_number):
 
 
 if __name__ == '__main__':
+    show_waiting_message = True
     ser = None
     while True:
         try:
@@ -73,69 +83,80 @@ if __name__ == '__main__':
                 ser = serial.Serial(port, baudrate, bytesize=bytesize, parity=parity, stopbits=stopbits)
                 if ser.is_open:
                     logging.info(f"Connected to {port}")
+                    show_waiting_message = True
                 else:
                     logging.warning("Failed to connect to card reader. Retrying in 5 seconds...")
                     time.sleep(5)
                     continue
 
-            logging.info("\nWaiting for data...")
-            line = ser.readline().decode('ascii', errors='ignore').strip()
-            logging.debug(f"Received line: {line}")
+            if show_waiting_message:
+                logging.info("\nWaiting for data...")
+                show_waiting_message = False
 
-            if "Em-Marine" in line:
-                try:
-                    card_number = line.split()[1].split(',')[1]
-                    logging.info(f"Card number read: {card_number}")
+            line = read_card_data_non_blocking(ser)
 
-                    user_name, last_checkin_time, current_time_iso, user_status, comment = get_and_patch_user_data(card_number)
+            if line:
+                logging.debug(f"Received line: {line}")
+                if "Em-Marine" in line:
+                    try:
+                        card_number = line.split()[1].split(',')[1]
+                        logging.info(f"Card number read: {card_number}")
 
-                    if user_name is None:
-                        logging.warning(f"User with card number {card_number} not found")
-                        continue
+                        user_name, last_checkin_time, current_time_iso, user_status, comment = get_and_patch_user_data(card_number)
 
-                    logging.info(f"User: {user_name}")
-                    activity = 'Вход' if user_status else 'Выход'
-                    logging.info(f"Activity: {activity}")
-                    if comment:
-                        logging.info(f"Comment: {comment}")
+                        if user_name is None:
+                            logging.warning(f"User with card number {card_number} not found")
+                            continue
 
-                    time_diff_formatted = "-"
+                        logging.info(f"User: {user_name}")
+                        activity = 'Вход' if user_status else 'Выход'
+                        logging.info(f"Activity: {activity}")
+                        if comment:
+                            logging.info(f"Comment: {comment}")
 
-                    if not user_status:  # Если user_status == False (т.е. "Выход")
-                        try:
-                            time_diff = datetime.now() - last_checkin_time
-                            time_diff_seconds = time_diff.total_seconds()
-                            hours = int(time_diff_seconds // 3600)
-                            minutes = int((time_diff_seconds % 3600) // 60)
-                            seconds = int(time_diff_seconds % 60)
-                            time_diff_formatted = f"{hours:02}:{minutes:02}:{seconds:02}"
-                            logging.debug(f"Time difference: {time_diff_formatted}")
+                        time_diff_formatted = "-"
 
-                        except (TypeError, ValueError) as e:
-                            logging.error(f"Error: {e}")
-                            time_diff_formatted = "-"
+                        if not user_status:  # Если user_status == False (т.е. "Выход")
+                            try:
+                                time_diff = datetime.now() - last_checkin_time
+                                time_diff_seconds = time_diff.total_seconds()
+                                hours = int(time_diff_seconds // 3600)
+                                minutes = int((time_diff_seconds % 3600) // 60)
+                                seconds = int(time_diff_seconds % 60)
+                                time_diff_formatted = f"{hours:02}:{minutes:02}:{seconds:02}"
+                                logging.debug(f"Time difference: {time_diff_formatted}")
 
-                    fields = {
-                        'NAME': card_number,                    # Название
-                        'PROPERTY_3246': user_name,             # ФИО
-                        'PROPERTY_3248': current_time_iso,      # Время регистрации ключа
-                        'PROPERTY_3348': activity,              # Активность
-                        'PROPERTY_3352': time_diff_formatted,   # Длительность
-                        'PROPERTY_3510': comment,               # Комментарий
-                    }
+                            except (TypeError, ValueError) as e:
+                                logging.error(f"Error: {e}")
+                                time_diff_formatted = "-"
 
-                    response = create_element(2025, fields)
-                    logging.debug(f"Data sent to Bitrix24: {response}")
+                        fields = {
+                            'NAME': card_number,                    # Название
+                            'PROPERTY_3246': user_name,             # ФИО
+                            'PROPERTY_3248': current_time_iso,      # Время регистрации ключа
+                            'PROPERTY_3348': activity,              # Активность
+                            'PROPERTY_3352': time_diff_formatted,   # Длительность
+                            'PROPERTY_3510': comment,               # Комментарий
+                        }
 
-                except (IndexError, KeyError, ValueError) as e:  # Объединяем обработку исключений
-                    logging.error(f"Error processing card data: {e}")
-                except requests.exceptions.RequestException as e:
-                    logging.exception(f"Error communicating with Bitrix24: {e}")
+                        response = create_element(2025, fields, timeout=TIMEOUT)
+                        if response is None:
+                            logging.error("Error communicating with Bitrix24")
+                        else:
+                            logging.debug(f"Data sent to Bitrix24: {response}")
 
-            elif "No card" in line:
-                print("\n...")
+                    except (IndexError, KeyError, ValueError) as e:  # Объединяем обработку исключений
+                        logging.error(f"Error processing card data: {e}")
+                    except requests.exceptions.RequestException as e:
+                        logging.exception(f"Error communicating with Bitrix24: {e}")
+
+                elif "No card" in line:
+                    print("\n...")
+                else:
+                    logging.warning("The card is not supported")
             else:
-                logging.warning("The card is not supported")
+                show_waiting_message = False
+                time.sleep(0.1)  # Пауза, чтобы не перегружать процессор
 
         except serial.SerialException as e:
             logging.error(f"Serial port error: {e}")
